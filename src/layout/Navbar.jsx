1b124bdd1d0bf7d8d3ad8f0ef90fbddbe3d3ec2d
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-19 21:56:53 -0500

    feat: adding svg icons to Navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className=" bg-cyan-900 flex justify-around items-center p-1">
        
         <Link to="/home" className="flex flex-col justify-center items-center">
-          <img className="" src="./icono-home.svg" width="40px" alt="icono-home" />
+          <img className="" src="./public/home-icon.svg" alt="home icon" />
           <span className="text-white text-sm font-anton hover:text-green-500">
             HOME
           </span>
@@ -15,7 +15,7 @@ const Navbar = () => {
 
         <Link to="/statistics" className="flex flex-col justify-center items-center" >
           <img
-            src="./icono-statistics.svg"
+            src="./public/search-icon.svg"
             width="40px"
             alt="icono-statistics"
           />
@@ -26,11 +26,19 @@ const Navbar = () => {
 
 
         <Link to="/profile" className="flex flex-col justify-center items-center">
-          <img src="./icono-profile.svg" width="40px" alt="icono-profile" />
+          <img src="./public/order-icon.svg" width="40px" alt="icono-profile" />
+          <span className="text-white text-sm font-anton hover:text-green-500">
+            ORDERS
+          </span>
+        </Link>
+
+        <Link to="/profile" className="flex flex-col justify-center items-center">
+          <img src="./public/profile-icon.svg" width="40px" alt="icono-profile" />
           <span className="text-white text-sm font-anton hover:text-green-500">
             PROFILE
           </span>
         </Link>
+
       </div>
     </div>
   );
