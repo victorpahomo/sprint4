5ce106cae685248878e26f5bee1ee464b8c0a6a1
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-22 02:28:11 -0500

    feat: Changed Logins BaseData bug for Login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider } from "firebase/auth"
 import { auth, db } from "../api/firebase";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 // Contexto
 const authContext = createContext();
 
@@ -23,28 +23,83 @@ export function AuthProvider({ children }) {
       const docuRef = doc(db,`users/${userInfo.user.uid}`);
       setDoc(docuRef, {email:email, rol:"user",orders:[] });
     }
-    // Iniciar sesion
-    const login =  async (email, password) => {
-       await signInWithEmailAndPassword(auth, email, password)
-    }
+
     // Cerrar sesion
     const logout =  () => {
         signOut(auth)
     }
+    const login = async (email, password) => {
+          // Obtiene una referencia al documento del usuario en Firestore
+      const docuRef = await signInWithEmailAndPassword(auth, email, password)
+      
+      // Comprueba si el usuario ya existe en Firestore
+      const docSnap = await getDoc(docuRef);
+      if (docSnap.exists()) {
+        // Si el usuario ya existe, no se crea un nuevo documento
+        console.log("El usuario ya está registrado en Firestore.");
+      } else {
+        // Si el usuario no existe, se crea un nuevo documento con su información
+        await setDoc(docuRef, {
+          email: userInfo.user.email,
+          rol: "user",
+          orders: []
+        });
+        console.log("Se ha creado un nuevo documento en Firestore para el usuario.");
+      }
+    };
 
+    // Iniciar sesion con Google 
     const loginWithGoogle = async () => {
-        const googleProvider = new GoogleAuthProvider();
-        const userInfo = await signInWithPopup(auth, googleProvider)
-        const docuRef = doc(db,`users/${userInfo.user.uid}`);
-        setDoc(docuRef, {email:userInfo.user.email, rol:"user",orders:[] });
-    }
+      // Crea una instancia del proveedor de autenticación de Google
+      const googleProvider = new GoogleAuthProvider();
+      
+      // Inicia sesión con Google
+      const userInfo = await signInWithPopup(auth, googleProvider)
+      
+      // Obtiene una referencia al documento del usuario en Firestore
+      const docuRef = doc(db,`users/${userInfo.user.uid}`);
+      
+      // Comprueba si el usuario ya existe en Firestore
+      const docSnap = await getDoc(docuRef);
+      if (docSnap.exists()) {
+        // Si el usuario ya existe, no se crea un nuevo documento
+        console.log("El usuario ya está registrado en Firestore.");
+      } else {
+        // Si el usuario no existe, se crea un nuevo documento con su información
+        await setDoc(docuRef, {
+          email: userInfo.user.email,
+          rol: "user",
+          orders: []
+        });
+        console.log("Se ha creado un nuevo documento en Firestore para el usuario.");
+      }
+    };
 
     const loginWithFacebook = async () => {
-        const facebookProvider = new FacebookAuthProvider();
-        const userInfo = await signInWithPopup(auth, facebookProvider)
-        const docuRef = doc(db,`users/${userInfo.user.uid}`);
-        setDoc(docuRef, {email:userInfo.user.email, rol:"user",orders:[] });
-        }
+      // Crea una instancia del proveedor de autenticación de Facebook
+      const facebookProvider = new FacebookAuthProvider();
+      
+      // Inicia sesión con Facebook
+      const userInfo = await signInWithPopup(auth, facebookProvider);
+      
+      // Obtiene una referencia al documento del usuario en Firestore
+      const docuRef = doc(db, `users/${userInfo.user.uid}`);
+      
+      // Comprueba si el usuario ya existe en Firestore
+      const docSnap = await getDoc(docuRef);
+      if (docSnap.exists()) {
+        // Si el usuario ya existe, no se crea un nuevo documento
+        console.log("El usuario ya está registrado en Firestore.");
+      } else {
+        // Si el usuario no existe, se crea un nuevo documento con su información
+        await setDoc(docuRef, {
+          email: userInfo.user.email,
+          rol: "user",
+          orders: []
+        });
+        console.log("Se ha creado un nuevo documento en Firestore para el usuario.");
+      }
+    };
 
 
     useEffect(() => {
