5247b4200f3e5ead8100e8f45dd7c68dd846b690
moralesvictorr <vmmorales@unicauca.edu.co>
2023-02-23 03:28:40 -0500

    feat: adding styles

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,35 +1,88 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useInfo } from '../context/HandleInfoContext'
-import { useData } from '../context/DataContext'
-const Checkout = () => {
-  let orders = []
-  orders = JSON.parse(localStorage.getItem('orders'))[0]
-  let total = localStorage.getItem("total")
-  const { setOrderToFirestore } = useData()
-  const { setpayCheck } = useInfo()
-  const navigate = useNavigate()
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useInfo } from "../context/HandleInfoContext";
+import { useData } from "../context/DataContext";
 
+const Checkout = () => {
+  let orders = [];
+  orders = JSON.parse(localStorage.getItem("orders"))[0];
+  let total = localStorage.getItem("total");
+  const { setOrderToFirestore } = useData();
+  const { setpayCheck } = useInfo();
+  const navigate = useNavigate();
+  const [totalOrder, setTotalOrder] = useState(
+    localStorage.getItem("total") ?? 0
+  );
   // Combinar los valores en un objeto
   const ordersWithTotal = {
     ...orders, // Obtenemos el primer y único objeto del array
-    total: total
+    total: total,
   };
 
-  console.log(ordersWithTotal);
-
   const handleClick = () => {
-    setpayCheck(true)
-    setOrderToFirestore(ordersWithTotal)
-    navigate('/orderaccepted')
-  }
+    setpayCheck(true);
+    setOrderToFirestore(ordersWithTotal);
+    navigate("/orderaccepted");
+  };
+
   return (
+    <div className=" p-4 md:p-8">
 
-    <div>
+      <img
+        className="w-1/2 h-22 md:w-80 mb-4"
+        src={ordersWithTotal.logo}
+        alt=""
+      />
 
-      <button onClick={handleClick}>Place Order</button>
+      <div className="flex gap-4 pb-4 md:">
+        {ordersWithTotal.dishes.map((object) => {
+          const id = object.id;
+          const image = object.image;
+          const name = object.name;
+          const price = object.price;
+          const quantity = object.quantity;
+          let totalOrderPrice = 0;
+          const totalUnitPrice = price * quantity;
+          totalOrderPrice += totalUnitPrice;
+          console.log((totalOrderPrice += totalUnitPrice));
+          return (
+            <div
+              key={id + Date.now()}
+              className="flex items-center justify-center hover:shadow-lg transform hover:-translate-y-1 transition-all duration-200 ease-in-out"
+            >
+              <div className="flex flex-col items-center justify-center w-full h-full cursor-pointer ">
+                <div className="flex items-center gap-8 w-screen">
+                  <img
+                    className="rounded-lg w-24 h-24 md:w-32 md:h-32"
+                    src={image}
+                    alt={name}
+                  />
+                  <div className="flex flex-col gap-5">
+                    <p className="text-slate-800">{name}</p>
+                    <div className="flex gap-1">
+                      <p className="text-slate-500">${price}</p>
+                      <p className="text-slate-500">x {quantity} =</p>
+                      <p className="text-yellow-700"> ${price * quantity}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        className="fixed bottom-0 left-0 right-0 mx-auto w-full h-12 md:h-16 bg-yellow-1000 hover:bg-yellow-400 text-slate-800 font-bold text-lg md:text-xl flex items-center justify-center transform hover:scale-105 transition-all duration-200 ease-in-out"
+        onClick={handleClick}
+      >
+        <div className="flex gap-2 items-center sm:w-1/2 sm:flex sm:justify-center sm:items-center">
+          <span>Place Order</span>
+          <div className="sm:fixed sm:right-5 bg-yellow-400 rounded-xl sm:p-1">
+            Total: ${totalOrder}
+          </div>
+        </div>
+      </button>
     </div>
-  )
-}
-
-export default Checkout
+  );
+};
+export default Checkout;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,7 +8,7 @@ const Search = () => {
   const [categoryToSearch, setCategoryToSearch] = useState('');
   const [dishesObtained, setDishesObtained] = useState([]);
   const [submitted, setSubmitted] = useState(false); // Variable para saber si se econtró el palto a buscar
-  const {setRestaurantSelected,setrestaurantToSend,setProductSelected} = useInfo();
+  const { setRestaurantSelected, setrestaurantToSend, setProductSelected } = useInfo();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -32,7 +32,7 @@ const Search = () => {
       image: dish.image,// agregar la imagen del plato al objeto
       price: dish.price,//  agregar el precio del plato al objeto
       idItem: dish.idItem,// agregar el id del plato al objeto
-      cookingTimeMin: dish.cookingTimeMin ,// agregar el tiempo de preparación mínimo del plato al objeto
+      cookingTimeMin: dish.cookingTimeMin,// agregar el tiempo de preparación mínimo del plato al objeto
       cookingTimeMax: dish.cookingTimeMax,// agregar el tiempo de preparación máximo del plato al objeto
       curr: curr// agregar el restaurante actual al objeto
     }));
@@ -49,71 +49,56 @@ const Search = () => {
   return (
     <div className="flex flex-col w-screen h-screen items-center justify-center pb-10">
       <div className="flex fixed top-8 h-full md:w-3/4 md:h-80 flex-col md:p-1">
-        <form
-          className="flex w-full h-10 items-center gap-1"
-          onSubmit={handleSubmit}
-        >
-          <label htmlFor="search" className="cursor-pointer">
-            <svg
-              className="fill-slate-400"
-              width="26"
-              height="26"
-              viewBox="0 0 26 26"
-              fill="inherit"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M18.7925 3.2243C16.7133 1.14507 13.9489 0 11.0084 0C8.06792 0 5.30349 1.14507 3.2243 3.2243C1.14507 5.30349 0 8.06792 0 11.0084C0 13.9488 1.14507 16.7133 3.2243 18.7925C5.30349 20.8717 8.06797 22.0168 11.0084 22.0168C13.9488 22.0168 16.7133 20.8717 18.7925 18.7925C20.8717 16.7133 22.0168 13.9488 22.0168 11.0084C22.0168 8.06792 20.8717 5.30349 18.7925 3.2243ZM11.0084 19.9162C6.09654 19.9162 2.10052 15.9202 2.10052 11.0084C2.10052 6.09654 6.09659 2.10052 11.0084 2.10052C15.9202 2.10052 19.9162 6.09654 19.9162 11.0084C19.9162 15.9202 15.9202 19.9162 11.0084 19.9162Z"
-                fill="inherit"
-              />
-              <path
-                d="M24.0975 24.3822L17.8677 18.1526C17.4976 17.7825 16.8976 17.7825 16.5275 18.1526C16.1575 18.5226 16.1575 19.1227 16.5275 19.4927L22.7572 25.7224C22.9423 25.9075 23.1848 26 23.4274 26C23.6699 26 23.9124 25.9075 24.0975 25.7224C24.4675 25.3523 24.4675 24.7523 24.0975 24.3822Z"
-                fill="inherit"
-              />
-            </svg>
-          </label>
-          <input
-            className="w-full h-full pl-2 rounded-lg cursor-pointer hover:bg-yellow-100 border border-slate-300"
-            type="search"
-            value={categoryToSearch}
-            onChange={handleChange}
-          />
+        <form className="flex w-full h-10 items-center gap-1" onSubmit={handleSubmit}>
+          <div className="relative w-full">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="inherit"><path d="M18.793 3.224A10.936 10.936 0 0 0 11.008 0c-2.94 0-5.705 1.145-7.784 3.224A10.936 10.936 0 0 0 0 11.008c0 2.94 1.145 5.705 3.224 7.785a10.936 10.936 0 0 0 7.784 3.224c2.94 0 5.705-1.145 7.785-3.224a10.936 10.936 0 0 0 3.224-7.785c0-2.94-1.145-5.705-3.224-7.784Zm-7.785 16.692c-4.911 0-8.907-3.996-8.907-8.908 0-4.911 3.996-8.907 8.907-8.907 4.912 0 8.908 3.996 8.908 8.907 0 4.912-3.996 8.908-8.908 8.908Z" /><path d="m24.098 24.382-6.23-6.23a.948.948 0 0 0-1.34 1.34l6.23 6.23a.945.945 0 0 0 1.34 0c.37-.37.37-.97 0-1.34Z" /></svg>
+            </span>
+            <input
+              className="w-full p-4 pl-10 bg-stone-100 border-none rounded-lg cursor-pointer hover:bg-stone-100 border border-stone-100"
+              type="search"
+              value={categoryToSearch}
+              onChange={handleChange}
+              placeholder="Search for a dish"
+            />
+          </div>
           <button
-            className="hover:bg-yellow-400 h-full text-slate-600 bg-yellow-1000 rounded-md p-1"
+            className="cursor-pointer hover:bg-yellow-200 p-4 text-slate-600 bg-yellow-1000 rounded-md"
             type="submit"
           >
             Search
           </button>
         </form>
+
         <div className="flex flex-col justify-center items-center mt-2">
           {submitted && filteredDishes.length
             ? filteredDishes.map((dish) => (
-                <div
-                  onClick={()=> handleClick(dish)}// al hacer clic en un plato, se establece el restaurante seleccionado en el contexto
-                  className="flex w-full h-14 justify-item items-center gap-2 ml-14"
-                  key={dish.name}
-                >
-                  <img
-                    className="w-20 h-12 rounded-xl"
-                    src={dish.image}
-                    alt={dish.name}
-                  />
-                  <div>
-                    <h3>{dish.name}</h3>
-                    <p className="text-slate-400">$ {dish.price}.00</p>
-                  </div>
+              <div
+                onClick={() => handleClick(dish)}// al hacer clic en un plato, se establece el restaurante seleccionado en el contexto
+                className="flex w-full h-14 justify-item items-center gap-2 ml-14"
+                key={dish.name}
+              >
+                <img
+                  className="w-20 h-12 rounded-xl"
+                  src={dish.image}
+                  alt={dish.name}
+                />
+                <div>
+                  <h3>{dish.name}</h3>
+                  <p className="text-slate-400">$ {dish.price}.00</p>
                 </div>
-              ))
+              </div>
+            ))
             : submitted && (
-                <div className="flex flex-col items-center justify-center">
-                  <img
-                    className="w-50"
-                    src="https://res.cloudinary.com/dxwzyjefd/image/upload/v1677027958/sprint4/gui-images/notfound.png"
-                    alt="not found"
-                  />
-                  <p>Nothing found</p>
-                </div>
-              )}
+              <div className="flex flex-col items-center justify-center">
+                <img
+                  className="w-50"
+                  src="https://res.cloudinary.com/dxwzyjefd/image/upload/v1677027958/sprint4/gui-images/notfound.png"
+                  alt="not found"
+                />
+                <p>Nothing found</p>
+              </div>
+            )}
         </div>
       </div>
       <Navbar />
